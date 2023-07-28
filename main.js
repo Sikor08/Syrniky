bbe38de17b83e4bc69992be6f7d49f0244c71a19
Sikor08 <sikor08@gmail.com>
2023-07-28 12:07:48 +0300

    styles modified

--- main.js
+++ main.js
@@ -6,46 +6,28 @@ burgerBtn.addEventListener('click', (e) => {
     burgerMenuList.classList.toggle('burgerList__open');
 });
 
-
-
-
-
-
 // cart
 const catalogGoodBtn = document.querySelectorAll('.catalogGood__btn');
 const cartList = document.querySelector('.cartList');
 const cart = document.querySelector('.cart');
 const cartQuantity = document.querySelector('.cart__quantity');
 const cartIcon = document.querySelector('.headerIcon__cart');
-const cartPlusBtn = document.querySelector('.cartStepperPlus');
-
 
 let goods = []
-
+let totalCostArr = []
 cartIcon.addEventListener('click', () => {
 printQuantity() > 0 ? cart.classList.toggle('displayNone') : cart.classList.add('displayNone');
-
 
 })
 
 const totalPrice = document.querySelector('.totalPriceValue');
-let price = 0;
 
 // накидываю id на товары каталога
 // const catalogGood = document.querySelectorAll('.catalogGood');
 // catalogGood.forEach(item => {
 //   item.id = crypto.randomUUID();
-// })
+// })sa
 
-const plusFullprice = (currentPrice) => {
-  return price += currentPrice;
-}
-const minusFullprice = (currentPrice) => {
-  return price -= currentPrice;
-}
-const printTotalPrice = () => {
-  totalPrice.textContent = `${price}`
-}
 const printQuantity = () => {
   let length = cartList.children.length
   cartQuantity.textContent = length;
@@ -55,37 +37,27 @@ const generateCartGood = (img, text, price, id) => {
   return `
   <li class="cartGood__item" >
    <article class="cartGood" data-id="${id}" data-price="${price}">
-    <div class="cartGood__img">
-      <img src="${img}" alt="">
-    </div>
-                        <div class="cartGoodInfo">
-                            <p class="cartGood__text">${text}</p>
-                            <div class="cartGoodPrice">
-                                <p class="cartGood__price">Цена: <span class="cartGood__priceValue">${price}</span> р</p>
-                            </div>
-                        </div>
-                        <div class="cartGoodStepper">
-                            <button class="cartStepperBtn cartStepperMinus" data-id="${id}">-</button>
-                            <span class="cartStepperValue">1</span>
-                            <button class="cartStepperBtn cartStepperPlus" data-id="${id}">+</button>
-
-                        </div>
-                        <div class="del">
-                            
-                                <img class="delBtn" src="img/delImg.svg" alt="">
-                          
-                        </div>
-                        </article>
-                    </li> 
+      <img class="cartGood__img" src="${img}" alt="">
+      <div class="cartGoodInfo">
+        <p class="cartGood__text">${text}</p>
+        <p class="cartGood__price">Цена: <span class="cartGood__priceValue">${price}</span> р</p>
+          </div>
+      <div class="cartGoodStepper">
+        <button class="cartStepperBtn cartStepperMinus">-</button>
+        <span class="cartStepperValue">1</span>
+        <button class="cartStepperBtn cartStepperPlus">+</button>
+          </div>
+      <div class="del">
+        <img class="delBtn" src="img/delImg.svg" alt="">
+          </div>
+      </article>
+    </li> 
   `
 }
 
 const deleteGoodFromCart = (productParent) => {
   let id = productParent.querySelector('.cartGood').dataset.id;
   document.querySelector(`.catalogGood[id="${id}"]`).querySelector('.catalogGood__btn').disabled = false;
-  let currentPrice = productParent.querySelector('.cartGood__priceValue').textContent;
-  minusFullprice(currentPrice);
-  printTotalPrice()
   productParent.remove();
   if(printQuantity() == 0) {
     cartQuantity.style.display = 'none'
@@ -100,9 +72,6 @@ catalogGoodBtn.forEach(el => {
     let img = parent.querySelector('.catalogGood__img').getAttribute('src');
     let title = parent.querySelector('.catalogGood__title').textContent;
     let price = parseInt(parent.querySelector('.catalogGood__priceValue').textContent);
-
-  
-
 
     cartList.insertAdjacentHTML('afterbegin', generateCartGood(img, title, price, id));
 
@@ -142,17 +111,16 @@ catalogGoodBtn.forEach(el => {
         cartMinusBtn.disabled = false;
 
         count ++;
-        if (count >= 4) {
+        if (count >= 9) {
           cartPlusBtn.disabled = true;
-          alert('Не объешься?')
         }
         stepperValue.textContent = count;
-       
-
+      
         priceGoodArr.push(priceGoodValue);
         let priceGoodsSum = priceGoodArr.reduce((sum, item) => sum + item) ;
         priceGood.textContent = priceGoodsSum;
-        let goodPrices = document.querySelectorAll('.cartGood__priceValue')
+        let goodPrices = document.querySelectorAll('.cartGood__priceValue');
+
         let totalCostArr = [];
 
         goodPrices.forEach(item => {
@@ -164,16 +132,11 @@ catalogGoodBtn.forEach(el => {
         totalPrice.textContent =totalCostSum
         
       })
-      cartMinusBtn.disabled = true;
-
-
-
+      
       cartMinusBtn.addEventListener('click', (e) => {
         cartPlusBtn.disabled = false
         count --;
-        if (count == 1) {
-          cartMinusBtn.disabled = true;
-        }
+
         stepperValue.textContent = count;
 
         let stepperQuantity = e.target.parentNode.querySelector('.cartStepperValue').textContent;
@@ -193,18 +156,14 @@ catalogGoodBtn.forEach(el => {
         });
         console.log(totalCostArr)
         let totalCostSum = totalCostArr.reduce((sum, item) => sum + item);
-        console.log(totalCostSum);
         totalPrice.textContent =totalCostSum
-
+        if (count < 1) {
+          deleteGoodFromCart(e.target.closest('.cartGood__item'));
+        }
       })
 }
     });
 });
-
-
-
-
-
 
 cartList.addEventListener('click', (e) => {
   if (e.target.classList.contains('delBtn')) {
@@ -216,12 +175,11 @@ cartList.addEventListener('click', (e) => {
         });
         console.log(totalCostArr)
         if (printQuantity() > 0) {
+          // let totalCostSum = totalCostArr.reduce((sum, item) => sum + item);
+          // console.log(totalCostSum);
           let totalCostSum = totalCostArr.reduce((sum, item) => sum + item);
-          console.log(totalCostSum);
           totalPrice.textContent =totalCostSum
-  
-        }
-        
+        } 
   }
 }
 )
